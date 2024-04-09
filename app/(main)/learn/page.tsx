81536da1badbc79eaa57d3bfe67e772f7e607f45
CarsonDev1 <buitritinht@gmail.com>
2024-04-09 09:14:44 +0700

    learn page wrapper, drizzle & neon db

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,12 +1,20 @@
 import Header from '@/app/(main)/learn/header';
 import FeedWrapper from '@/components/feed-wrapper';
 import StickyWrapper from '@/components/sticky-wrapper';
+import UserProgress from '@/components/user-progress';
 import React from 'react';
 
 const LearnPage = () => {
 	return (
 		<div className='flex flex-row-reverse gap-[48px] px-6'>
-			<StickyWrapper>My sticky sidebar</StickyWrapper>
+			<StickyWrapper>
+				<UserProgress
+					activeCourse={{ title: 'British', imageSrc: '/us.svg' }}
+					hearts={5}
+					points={100}
+					hasActiveSubscription={false}
+				/>
+			</StickyWrapper>
 			<FeedWrapper>
 				<Header title='British' />
 			</FeedWrapper>
